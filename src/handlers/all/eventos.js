f009055ee6e66edebcf009271b99612f069ed14b
Thekuruku <thekuruky@gmail.com>
2026-04-05 16:04:34 -0600

    Update eventos.js

--- src/handlers/all/eventos.js
+++ src/handlers/all/eventos.js
@@ -2,7 +2,7 @@ const { readdirSync } = require("node:fs");
 
 module.exports = (client) => {
     client.eventos = async () => {
-        /* Eventos */
+
         for (const subFolder of readdirSync(`${process.cwd()}/src/eventos/`)) {
             for (const fileName of readdirSync(`${process.cwd()}/src/eventos/${subFolder}/`)) {
                 let file = require(`${process.cwd()}/src/eventos/${subFolder}/${fileName}`);
